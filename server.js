47928b2911ef9468963452b60a40cb0f596839fb
CEO of Racism <66262879+menits12345@users.noreply.github.com>
2022-08-16 01:11:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.json());
 app.use(require("./routes/record"));
 // use the express-static middleware
 app.use(express.static("public"))
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 // get driver connection
 const dbo = require("./db/conn");
