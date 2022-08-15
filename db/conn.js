c1305f602e0083e0ebd480ad869ec9bd7c8e0529
Meni Tslakishvili <7450287-A_Depressed_Programmer@users.noreply.gitlab.com>
2022-08-15 16:41:12 +0300

    s

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
+const Db = "mongodb+srv://Meni:menits12345pass@cluster0.bisi6.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(Db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
