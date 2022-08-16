0e67628712a17768157927875af2afc44b4701a1
Meni Tslakishvili <7450287-A_Depressed_Programmer@users.noreply.gitlab.com>
2022-08-16 18:55:53 +0300

    little fix

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 var bodyParser = require('body-parser');
-const app = express();
+var app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // use the express-static middleware
+
+var getRawBody = require('raw-body')
+
 app.use(express.static("public"));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
