734d53cee129be5c1c4be18fcf625c4fbd1b1986
Meni Tslakishvili <7450287-A_Depressed_Programmer@users.noreply.gitlab.com>
2022-08-21 19:50:00 +0300

    little fix

--- routes/record.js
+++ routes/record.js
@@ -16,10 +16,13 @@ const JSRSASign = require('jsrsasign');
 //const mongoose = require("mongoose");
 const key = '$JesseWeNeedToCook!';
 var jsonParser = bodyParser.json({ limit: '5mb' });
+
 var urlencodedParser = bodyParser.urlencoded({ limit: '5mb', extended: true });
 recordRoutes.route("/").get(function (req, res) {
     res.json({ status: 'success', limit: bodyParser.limit });
 });
+
+
 
 // This section will help you check if user with name exists.
 // no check of token here
